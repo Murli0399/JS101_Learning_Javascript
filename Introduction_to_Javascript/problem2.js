2fc57d8d7b3348878acd53664c4bfb83d02ae2c5
Murli0399 <murlikhaire28@gmail.com>
2022-10-11 08:52:25 +0530

    problem1

---
+++ Introduction_to_Javascript/problem2.js
@@ -0,0 +1,6 @@
+let name = "Murli Khaire";
+console.log(name);
+let father_name = "Badri Prasad Khaire";
+console.log(father_name);
+let mother_name = "Usha Khaire";
+console.log(mother_name);
---
+++ Introduction_to_Javascript/problem4.js
@@ -0,0 +1,23 @@
+
+let name = "Name : Murli";
+console.log(name);
+let school = "School : Sofia School";
+console.log(school);
+let grade = "Grade : A+";
+console.log(grade);
+let section = "Section : A";
+console.log(section);
+let roll_no = 101;
+console.log("Roll No :",roll_no);
+let marks = "@ 3 subject marks @";
+console.log(marks);
+
+let sub1 = "Hindi = 89";
+console.log(sub1);
+
+let sub2 = "Emglish = 81";
+console.log(sub2);
+
+let sub3 = "Maths = 95";
+console.log(sub3);
+
